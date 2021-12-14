de5e4d303acd99e14f79a2c107ee21ad4fcd1759
David Subiros <subiros.david@gmail.com>
2021-12-14 12:53:52 +0000

    set default kafka addr to 3 brokers

--- config/config.go
+++ config/config.go
@@ -48,7 +48,7 @@ func Get() (*Config, error) {
 		GracefulShutdownTimeout:    5 * time.Second,
 		HealthCheckInterval:        30 * time.Second,
 		HealthCheckCriticalTimeout: 90 * time.Second,
-		KafkaAddr:                  []string{"localhost:9092"},
+		KafkaAddr:                  []string{"localhost:9092", "localhost:9093", "localhost:9094"},
 		KafkaVersion:               "1.0.2",
 		KafkaOffsetOldest:          true,
 		KafkaNumWorkers:            1,
