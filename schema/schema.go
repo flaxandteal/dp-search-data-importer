97fd7f77da0b6f2ba775f4a8a28346ba691f4845
bpathak-ons <bhupendra.pathak@ext.ons.gov.uk>
2022-02-08 18:15:04 +0000

    review updates

--- schema/schema.go
+++ schema/schema.go
@@ -14,12 +14,12 @@ var searchDataImportEvent = `{
     {"name": "search_index", "type": "string", "default": ""},
     {"name": "cdid", "type": "string", "default": ""},
     {"name": "dataset_id", "type": "string", "default": ""},
-    {"name": "keywords","type":["null",{"type":"array","items":"string"}]},
+    {"name": "keywords","type": ["null",{"type":"array","items":"string"}]},
     {"name": "meta_description", "type": "string", "default": ""},
     {"name": "release_date", "type": "string", "default": ""},
     {"name": "summary", "type": "string", "default": ""},
     {"name": "title", "type": "string", "default": ""},
-    {"name": "topics","type":["null",{"type":"array","items":"string"}]},
+    {"name": "topics", "type":"array", "items":"string"},
     {"name": "trace_id", "type": "string", "default": ""}
   ]
 }`
