65af133146af64ef58a1f71c60d4d4d3883f9f18
bpathak-ons <bhupendra.pathak@ext.ons.gov.uk>
2022-02-08 18:20:08 +0000

    fix CI failure

--- schema/schema.go
+++ schema/schema.go
@@ -19,7 +19,7 @@ var searchDataImportEvent = `{
     {"name": "release_date", "type": "string", "default": ""},
     {"name": "summary", "type": "string", "default": ""},
     {"name": "title", "type": "string", "default": ""},
-    {"name": "topics", "type":"array", "items":"string"},
+    {"name": "topics", "type": {"type":"array","items":"string"}},
     {"name": "trace_id", "type": "string", "default": ""}
   ]
 }`
